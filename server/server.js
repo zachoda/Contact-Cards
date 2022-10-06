16e8735376f8db05ca25fed7e6ee457f6901fcd9
Zach Oda <zjodachow@gmail.com>
2022-10-06 11:36:26 -0400

    concurrently added

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static('../client'));
+
+require('./routes/htmlRoutes')(app);
+
+app.listen(PORT, function() {
+    console.log(`Now listening on port: ${PORT}`);
+});
