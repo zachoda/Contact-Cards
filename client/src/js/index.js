94f9c2dbb55c76b2045314743f30c0c558655c85
Zach Oda <zjodachow@gmail.com>
2022-10-06 15:16:00 -0400

    added database

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,5 +1,6 @@
 import  "./form";
 import "./submit";
+import {initdb} from './database';
 
 import "../css/index.css";
 
@@ -11,10 +12,13 @@ import Bear from '../images/bear.png';
 import Dog from '../images/dog.png';
 
 window.addEventListener('load', function() {
+    initdb();
     document.getElementById('logo').src = Logo;
     document.getElementById('bearThumbnail').src = Bear;
     document.getElementById('dogThumbnail').src = Dog;
 });
+
+
 
    
 
