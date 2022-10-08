47ba3032bdfe7e3160b502ba3713e7eadb148881
Zach Oda <zjodachow@gmail.com>
2022-10-08 11:29:39 -0400

    heroku staging

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -98,8 +98,20 @@ if('serviceWorker' in navigator) {
     navigator.serviceWorker.register('./service-worker.js');
   })
 };
+const installBtn = document.getElementById('installBtn');
+window.addEventListener('beforeinstallprompt', (event) => {
+  event.preventDefault();
+  installBtn.style.visibility = 'visible';
 
-
+  installBtn.addEventListener('click', () => {
+    event.prompt();
+    installBtn.setAttribute('disabled', true);
+    installBtn.textContent = 'Installed.';
+  });
+});
+window.addEventListener('appinstalled', (event) => {
+  console.log('Good.', 'appinstalled', event);
+});
 
    
 
